e19b5da20a6a3700a64b1fd9531824694f522c6b
Grégoire André <gregoir.andre@gmail.com>
2022-07-26 15:22:00 +0200

    Revert to always making a windows build

--- src/Main.hx
+++ src/Main.hx
@@ -308,6 +308,18 @@ class Main {
 					}
 				}
 				else {
+					// SDL Windows 64bits
+					makeHl(baseRedistDir+"/opengl_win/"+projectName, HL_RUNTIME_FILES_WIN, false);
+					if( zipping )
+						zipFolder( '$baseRedistDir/${projectName}_opengl_win.zip', baseRedistDir+"/opengl_win/");
+
+					// SDL Windows 32bits
+					if( hasParameter("-hl32") ) {
+						makeHl(baseRedistDir+"/opengl_win32/"+projectName, HL_RUNTIME_FILES_WIN, true);
+						if( zipping )
+							zipFolder( '$baseRedistDir/${projectName}_opengl_win32.zip', baseRedistDir+"/opengl_win32/");
+					}
+
 					// SDL Mac
 					// if( hasParameter("-mac") ) {
 					// 	makeHl(baseRedistDir+"/opengl_mac/"+projectName, HL_RUNTIME_FILES_MAC, false);
@@ -320,18 +332,6 @@ class Main {
 						makeHl(baseRedistDir+"/opengl_linux/"+projectName, HL_RUNTIME_FILES_LINUX, false);
 						if( zipping )
 							zipFolder( '$baseRedistDir/${projectName}_opengl_linux.zip', baseRedistDir+"/opengl_linux/");
-					} else {
-						// SDL Windows 64bits
-						makeHl(baseRedistDir+"/opengl_win/"+projectName, HL_RUNTIME_FILES_WIN, false);
-						if( zipping )
-							zipFolder( '$baseRedistDir/${projectName}_opengl_win.zip', baseRedistDir+"/opengl_win/");
-	
-						// SDL Windows 32bits
-						if( hasParameter("-hl32") ) {
-							makeHl(baseRedistDir+"/opengl_win32/"+projectName, HL_RUNTIME_FILES_WIN, true);
-							if( zipping )
-								zipFolder( '$baseRedistDir/${projectName}_opengl_win32.zip', baseRedistDir+"/opengl_win32/");
-						}
 					}
 				}
 			}
